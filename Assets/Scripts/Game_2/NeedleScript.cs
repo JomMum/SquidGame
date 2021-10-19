47809abb6242244879af06f9ed1fbc0fa550752b
JongWoong <thdwhddnd427@naver.com>
2021-10-19 21:50:22 +0900

    달고나 게임 일부 제작

---
+++ Assets/Scripts/Game_2/NeedleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleScript : MonoBehaviour
+{
+    public int moveSpd;
+
+    void Update()
+    {
+        int randomX = Random.Range(230, -230);
+        int randomY = Random.Range(340, -135);
+
+        Vector2.MoveTowards(transform.localPosition, new Vector2(randomX, randomY), moveSpd);
+    }
+}
