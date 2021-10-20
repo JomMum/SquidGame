cbfc4cc3daea9576875df9116fadc0caa7a8bc02
JongWoong <thdwhddnd427@naver.com>
2021-10-20 22:32:22 +0900

    줄다리기 게임 완성
    1. 패턴 설정 및 제한시간 감소
    2. 제한시간 내에 패턴 입력 시 성공
    3. 0점이 되면 아웃, 10점이 되면 클리어
    4. 패턴, 제한시간, 결과 UI 임시 적용

---
+++ Assets/Scripts/Game_3/Game3_Manager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game3_Manager : MonoBehaviour
+{
+    int tugOfWarPoint = 5; //줄다리기 포인트
+    public int limitTime; //제한시간
+    public int maxLimitTime = 50; //최대 제한시간
+
+    public int[] pattern = new int[15]; //입력 패턴
+    int curPattern; //현재 패턴
+
+    public bool canTouch; //터치 가능한 상태인가
+    public bool isClear; //해당 패턴을 클리어했는가
+
+    void Start()
+    {
+        tugOfWarPoint = 5;
+        WaveSetting();
+    }
+
+    void Update()
+    {
+        //게임 승패 판정
+        if (tugOfWarPoint >= 10)
+            Debug.Log("성공");
+        else if (tugOfWarPoint <= 0)
+            Debug.Log("실패");
+
+        if(canTouch)
+        {
+            //실패/통과 유무 판정
+            if (pattern[curPattern] == 0)
+            {
+                canTouch = false;
+                isClear = true;
+            }
+            else if ((Input.GetKeyUp(KeyCode.LeftArrow) && pattern[curPattern] != 1)
+                     || (Input.GetKeyUp(KeyCode.RightArrow) && pattern[curPattern] != 2)
+                     || (Input.GetKeyUp(KeyCode.UpArrow) && pattern[curPattern] != 3)
+                     || (Input.GetKeyUp(KeyCode.DownArrow) && pattern[curPattern] != 4))
+            {
+                canTouch = false;
+                isClear = false;
+            }
+
+            //올바르게 키를 눌렀을 시
+            if ((Input.GetKeyUp(KeyCode.LeftArrow) && pattern[curPattern] == 1)
+                || (Input.GetKeyUp(KeyCode.RightArrow) && pattern[curPattern] == 2)
+                || (Input.GetKeyUp(KeyCode.UpArrow) && pattern[curPattern] == 3)
+                || (Input.GetKeyUp(KeyCode.DownArrow) && pattern[curPattern] == 4))
+            {
+                curPattern++;
+            }
+        }
+    }
+
+    void WaveSetting()
+    {
+        curPattern = 0;
+        SetPattern();
+
+        canTouch = true;
+        StartCoroutine(StartLimitTime());
+    }
+
+    void SetPattern()
+    {
+        int patternLength = Random.Range(5, 11); //패턴 개수 결정
+
+        //랜덤한 패턴 코드 저장
+        for (int i=0; i<pattern.Length; i++)
+        {
+            if (i < patternLength)
+            {
+                int patternCode = Random.Range(1, 5);
+                pattern[i] = patternCode;
+            }
+            else
+                pattern[i] = 0;
+        }
+    }
+
+    IEnumerator StartLimitTime()
+    {
+        for(limitTime = maxLimitTime; limitTime > 0; limitTime--)
+            yield return new WaitForSeconds(0.1f);
+
+        canTouch = false;
+
+        //성공 판정
+        if (isClear)
+        {
+            tugOfWarPoint++;
+        }
+        else
+        {
+            tugOfWarPoint--;
+        }
+
+        isClear = false;
+        WaveSetting();
+    }
+}
---
+++ Assets/Scripts/Game_3/Game3_UIManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game3_UIManager : MonoBehaviour
+{
+    public Game3_Manager gameManager;
+    public Text patternText;
+    public Text resultUI;
+    public Image LimitTimeUI;
+
+    string patternName;
+
+    void Update()
+    {
+        //패턴 UI
+        if (gameManager.canTouch)
+            ShowPatternUI();
+
+        //결과창 UI
+        ShowResultUI();
+
+        //제한시간 UI
+        ShowLimitTimeUI();
+    }
+
+    void ShowPatternUI()
+    {
+        patternText.text = "";
+
+        for (int i = 0; i < gameManager.pattern.Length; i++)
+        {
+            //패턴 UI 표시
+            switch (gameManager.pattern[i])
+            {
+                case 1:
+                    patternName = "좌";
+                    break;
+                case 2:
+                    patternName = "우";
+                    break;
+                case 3:
+                    patternName = "상";
+                    break;
+                case 4:
+                    patternName = "하";
+                    break;
+                default:
+                    patternName = "";
+                    break;
+            }
+
+            patternText.text += patternName;
+        }
+    }
+
+    void ShowResultUI()
+    {
+        if (!gameManager.canTouch)
+        {
+            if (gameManager.isClear)
+                resultUI.text = "성공!";
+            else
+                resultUI.text = "실패!";
+        }
+        else
+        {
+            resultUI.text = "";
+        }
+    }
+
+    void ShowLimitTimeUI()
+    {
+        LimitTimeUI.fillAmount = (float)gameManager.limitTime / (float)gameManager.maxLimitTime;
+    }
+}
