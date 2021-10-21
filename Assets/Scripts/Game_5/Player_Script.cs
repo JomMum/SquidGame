98cbf76f2b887d2b791c1cfc2369d29959f986c1
terrard8 <92711313+terrard8@users.noreply.github.com>
2021-10-21 18:01:27 +0900

    Glass_game 완성

--- Assets/Scripts/Game_5/Player_Script.cs
+++ Assets/Scripts/Game_5/Player_Script.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player_Script : MonoBehaviour
 {
+    bool isleft;
+    bool isright;
+    float curY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +17,50 @@ public class Player_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            //Vector2.MoveTowards                             
+            isleft = false;
+            isright = false;
 
+            curY = transform.position.y;
+            isleft = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isleft = false;
+            isright = false;
+
+            curY = transform.position.y;
+            isright = true;
+        }
+
+        if (isleft)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(-1, curY + 2), 0.1f);
+        }
+        else if (isright)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(1, curY + 2), 0.1f);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Glass"))
+        {
+            if (transform.position.y >= curY + 2)
+            {
+                Glass_script glassScript = collision.gameObject.GetComponent<Glass_script>();
+
+                if(glassScript.ishard)
+                {
+                    Debug.Log("성공");
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
